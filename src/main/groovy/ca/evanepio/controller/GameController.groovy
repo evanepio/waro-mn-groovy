ccd5fea66cab72198bc717419b70571f22389187
Evan Porter <evan.porter@dotman.ca>
2019-03-25 19:57:25 -0300

    Adding a first-class client because why not

--- src/main/groovy/ca/evanepio/controller/GameController.groovy
+++ src/main/groovy/ca/evanepio/controller/GameController.groovy
@@ -1,5 +1,6 @@
 package ca.evanepio.controller
 
+import ca.evanepio.api.GameApi
 import ca.evanepio.domain.Game
 import groovy.transform.CompileStatic
 import io.micronaut.http.MediaType
@@ -8,7 +9,7 @@ import io.micronaut.http.annotation.Post
 
 @CompileStatic
 @Controller('/game')
-class GameController {
+class GameController implements GameApi {
 
     @Post(produces = MediaType.APPLICATION_JSON)
     Game create() {
